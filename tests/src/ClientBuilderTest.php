9057937750c4300e1d8aa4268f9165fe15d3efcc
Adrian Lorenc <adrian.lorenc@gmail.com>
2023-06-02 13:53:04 +0200

    Travis

---
+++ tests/src/ClientBuilderTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace PiwikPro\ReportingApi\tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Tests for the ClientBuilder class.
+ *
+ * @coversDefaultClass \PiwikPro\ReportingApi\Builder\ClientBuilder
+ */
+class ClientBuilderTest extends TestCase
+{
+    public function testDummy(): void
+    {
+        $number = 1;
+        $this->assertSame(0, $number);
+    }
+}
