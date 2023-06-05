4d3dbd415aec8bb2fa39bc1cd519413a4b8971ab
Adrian Lorenc <adrian.lorenc@gmail.com>
2023-06-05 10:44:57 +0200

    Basic test for client

--- tests/src/ClientBuilderTest.php
+++ tests/src/ClientBuilderTest.php
@@ -4,7 +4,15 @@ declare(strict_types=1);
 
 namespace PiwikPro\ReportingApi\tests;
 
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
+use PiwikPro\ReportingApi\Builder\ClientBuilder;
+use PiwikPro\ReportingApi\Client as PiwikProClient;
+use PiwikPro\ReportingApi\ClientInterface;
+use PiwikPro\ReportingApi\Query\DirectQuery;
 
 /**
  * Tests for the ClientBuilder class.
@@ -13,9 +21,38 @@ use PHPUnit\Framework\TestCase;
  */
 class ClientBuilderTest extends TestCase
 {
-    public function testDummy(): void
+    /**
+     * Test client creation.
+     */
+    public function testCreation(): void
     {
-        $number = 1;
-        $this->assertSame(1, $number);
+        $client = (ClientBuilder::create(
+            'https://github.com/adrian-lorenc/piwik-pro-reporting-api',
+            'client_id',
+            'client_secret'
+        ))->buildClient();
+
+        $this->assertInstanceOf(ClientInterface::class, $client);
+    }
+
+    /**
+     * Test request.
+     */
+    public function testRequest(): void
+    {
+        // Create a mock and queue two responses.
+        $mock = new MockHandler([
+            new Response(200, [], 'Hello, World'),
+        ]);
+
+        $handlerStack = HandlerStack::create($mock);
+        $guzzleClient = new GuzzleClient(['handler' => $handlerStack]);
+
+        $client = new PiwikProClient($guzzleClient);
+        $query = new DirectQuery('bzSmbxbKUu');
+
+        $response = $client->request($query);
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertSame(200, $response->getStatusCode());
     }
 }
